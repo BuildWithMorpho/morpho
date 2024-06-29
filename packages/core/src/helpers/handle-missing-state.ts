996b42889b50edb2efd7f4225d544857c69a77e2
BuildWithMorpho <junaidgohan123@gmail.com>
2024-06-29 02:57:33 -0700

    support httpRequests in morpho

---
+++ packages/core/src/helpers/handle-missing-state.ts
@@ -0,0 +1,11 @@
+import { MorphoComponent } from '..';
+import { getStateUsed } from './get-state-used';
+
+export function handleMissingState(json: MorphoComponent) {
+  const stateUsed = getStateUsed(json);
+  Array.from(stateUsed).forEach((property) => {
+    if (!(property in json.state)) {
+      json.state[property] = null;
+    }
+  });
+}
