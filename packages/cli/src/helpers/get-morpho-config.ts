9e7b5b540cbfb08b46aa46a0c649324838560819
BuildWithMorpho <junaidgohan123@gmail.com>
2024-08-15 08:24:42 +0300

    read generator config from morpho.config.js (#262)
    * read generator config from morpho.config.js
    
    * check for config file existance
    
    * Update packages/cli/src/commands/compile.ts
    
    
    * fix merging plugins

--- packages/cli/src/helpers/get-morpho-config.ts
+++ packages/cli/src/helpers/get-morpho-config.ts
@@ -1,6 +1,13 @@
+import fs from 'fs';
 import { MorphoConfig } from '@builder.io/morpho';
 
 export function getMorphoConfig(): MorphoConfig | null {
-  const module = require(process.cwd() + '/morpho.config');
-  return module?.default || module || null;
+  const path = process.cwd() + '/morpho.config';
+
+  if (fs.existsSync(path)) {
+    const module = require(path);
+    return module?.default || module || null;
+  }
+
+  return null;
 }
