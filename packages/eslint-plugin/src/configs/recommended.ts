75d13c564a35a775afbda6f49b476359b34708f5
BuildWithMorpho <junaidgohan123@gmail.com>
2024-08-30 12:44:54 -0400

    Misc: ESLint plugin testing (#456)
    * fix: root eslint config
    
    * examples: add eslint plugin to example
    
    * docs(eslint): document recommended config
    
    * chore: remove commented out code
    
    * fix buggy name
    
    * chore(eslint): move rule into file
    
    * fix(eslint): strongly type recommended rules to match rules
    
    * fix(tests): move test and fix import path
    
    * publish eslint plugin
    
    * publish eslint plugin

--- packages/eslint-plugin/src/configs/recommended.ts
+++ packages/eslint-plugin/src/configs/recommended.ts
@@ -1,24 +1,33 @@
+import { rules } from '../rules';
+
+const PLUGIN_NAME = '@builder.io/morpho' as const;
+
+type RulesKeys = `${typeof PLUGIN_NAME}/${keyof typeof rules}`;
+
+const recommendedRules: Record<RulesKeys, string> = {
+  '@builder.io/morpho/css-no-vars': 'error',
+  '@builder.io/morpho/jsx-callback-arg-name': 'error',
+  '@builder.io/morpho/jsx-callback-arrow-function': 'error',
+  '@builder.io/morpho/no-assign-props-to-state': 'error',
+  '@builder.io/morpho/no-async-methods-on-state': 'error',
+  '@builder.io/morpho/no-conditional-logic-in-component-render': 'error',
+  '@builder.io/morpho/no-state-destructuring': 'error',
+  '@builder.io/morpho/no-var-declaration-in-jsx': 'error',
+  '@builder.io/morpho/no-var-declaration-or-assignment-in-component': 'error',
+  '@builder.io/morpho/no-var-name-same-as-state-property': 'error',
+  '@builder.io/morpho/only-default-function-and-imports': 'error',
+  '@builder.io/morpho/ref-no-current': 'error',
+  '@builder.io/morpho/use-state-var-declarator': 'error',
+  '@builder.io/morpho/static-control-flow': 'error',
+  '@builder.io/morpho/no-var-name-same-as-prop-name': 'error',
+};
+
 export default {
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ['@builder.io/morpho'],
-  rules: {
-    '@builder.io/morpho/css-no-vars': 'error',
-    '@builder.io/morpho/jsx-callback-arg-name': 'error',
-    '@builder.io/morpho/jsx-callback-arrow-function': 'error',
-    '@builder.io/morpho/no-assign-props-to-state': 'error',
-    '@builder.io/morpho/no-async-methods-on-state': 'error',
-    '@builder.io/morpho/no-conditional-logic-in-component-render': 'error',
-    '@builder.io/morpho/no-state-destructuring': 'error',
-    '@builder.io/morpho/no-var-declaration-in-jsx': 'error',
-    '@builder.io/morpho/no-var-declaration-or-assignment-in-component':
-      'error',
-    '@builder.io/morpho/no-var-name-same-as-state-property': 'error',
-    '@builder.io/morpho/only-default-function-and-imports': 'error',
-    '@builder.io/morpho/ref-no-current': 'error',
-    '@builder.io/morpho/use-state-var-declarator': 'error',
-  },
+  plugins: [PLUGIN_NAME],
+  rules: recommendedRules,
 };
