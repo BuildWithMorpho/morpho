d6a5a802cb8f015d5647942722598c1cda5d0b26
BuildWithMorpho <junaidgohan123@gmail.com>
2024-08-30 00:12:06 -0400

    Revert "feat: include local export file (#449)" (#452)
    This reverts commit 2a64fbabad15c44eb3316368e6d374edc81247b6.

--- packages/core/src/types/morpho-component.ts
+++ packages/core/src/types/morpho-component.ts
@@ -49,19 +49,10 @@ export type MorphoComponentInput = {
   defaultValue: any;
 };
 
-export type MorphoExport = {
-  [name: string]: {
-    code: string;
-    usedInLocal?: boolean;
-    isFunction?: boolean;
-  };
-};
-
 export type MorphoComponent = {
   '@type': '@builder.io/morpho/component';
   name: string;
   imports: MorphoImport[];
-  exports?: MorphoExport;
   meta: JSONObject & {
     useMetadata?: JSONObject;
   };
