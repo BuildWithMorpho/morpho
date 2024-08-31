d4173425e08b78fb7ccf656af31ac6a61278a2e5
BuildWithMorpho <junaidgohan123@gmail.com>
2024-09-01 04:42:21 +0800

    fix: swap props test filename (#477)

--- packages/core/src/__tests__/data/basic-props.raw.tsx
+++ packages/core/src/__tests__/data/basic-props.raw.tsx
@@ -1,14 +1,14 @@
 import { useState } from '@builder.io/morpho';
 
 // @ts-ignore
-export default function MyBasicComponent({ children: c, type }) {
+export default function MyBasicComponent(props) {
   const state = useState({
     name: 'Decadef20',
   });
 
   return (
     <div>
-      {c} {type}
+      {props.children} {props.type}
       Hello! I can run in React, Vue, Solid, or Liquid!
     </div>
   );
