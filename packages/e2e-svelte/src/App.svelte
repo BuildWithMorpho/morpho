2ca88c0332b2251b2ba2f06fef477ec1768b943b
BuildWithMorpho <junaidgohan123@gmail.com>
2024-09-04 13:15:28 -0500

    Generate and bundle-for-package in e2e-app (#513)
    * First approach to allow some targets to fail in e2e-app
    
    * E2E app: avoid output path conflict between concurrent runs
    
    * React E2E: consume Morpho output as package
    
    * Svelte E2E: Consume Morpho output as package
    
    * Solid E2E: Consume Morpho output as package

---
+++ packages/e2e-svelte/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  // Morpho generated Svelte JavaScript components; we are testing from a
+  //TypeScript app, so we ts-ignore when importing:
+
+  // @ts-ignore
+  import MyComponent from '@builder.io/e2e-app/svelte/components/my-component.svelte';
+
+  // Morpho could emit TypeScript Svelte in the future, ideally following the
+  // Svelte guidance:
+  // https://github.com/sveltejs/language-tools/blob/master/docs/preprocessors/typescript.md#typing-components-authoring-packages
+</script>
+
+<MyComponent></MyComponent>
