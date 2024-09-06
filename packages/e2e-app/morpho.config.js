39aabc82902bd4547a51eb2e99d46c56e37a66f0
BuildWithMorpho <junaidgohan123@gmail.com>
2024-09-06 00:34:54 -0400

    Fixes: stop emitting TS, CSS binding merge (#534)
    * stop emitting ts by default
    
    * fix css bindings merge
    
    * update snapshots
    
    * fix prop name
    
    * fix e2e-app config
    
    * fix e2e app config

--- packages/e2e-app/morpho.config.js
+++ packages/e2e-app/morpho.config.js
@@ -1,3 +1,6 @@
+/**
+ * @type {import('@builder.io/morpho'.MorphoConfig)}
+ */
 module.exports = {
   files: 'src/**',
   // A real Morpho application usually has a set of targets to include here;
@@ -7,6 +10,7 @@ module.exports = {
   // Each run needs a separate output dest, as Morpho clears it first.
   dest: ['output/' + process.argv[3]],
   options: {
-    react: { transpiler: { format: 'esm' } },
+    react: { transpiler: { format: 'esm', languages: ['ts'] } },
+    solid: { transpiler: { languages: ['ts'] } },
   },
 };
