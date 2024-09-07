c0eed18ce79d160f4129747e5c9afd46f3f5a058
BuildWithMorpho <junaidgohan123@gmail.com>
2024-09-07 11:35:01 -0400

    Fixes: Svelte (#556)
    * handle keyed arrays
    
    * add missing tests
    
    * update snapshots
    
    * improve is-children check for svelte
    
    * update prettier svelte plugin
    
    * fix comment
    
    * use For scope
    
    * update snapshots
    
    * fix on: handling
    
    * refactor isSlotProperty
    
    * update snapsshots
    
    * add missing yarn.lock changes

--- packages/core/src/helpers/is-children.ts
+++ packages/core/src/helpers/is-children.ts
@@ -1,5 +1,8 @@
 import { MorphoNode } from '../types/morpho-node';
 
 export default function isChildren(node: MorphoNode): boolean {
-  return `${node.bindings._text?.code || ''}`.replace(/\s+/g, '') === 'props.children';
+  return (
+    `${node.bindings._text?.code || node.properties.__text || ''}`.replace(/\s+/g, '') ===
+    'props.children'
+  );
 }
