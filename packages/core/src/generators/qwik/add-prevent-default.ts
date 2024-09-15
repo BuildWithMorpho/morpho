6fddec0377d7bcfdb98a00070611b629d53b3d35
BuildWithMorpho <junaidgohan123@gmail.com>
2024-09-14 18:15:18 -0700

    allow angular to reference external imported values in templates (#625)
    * allow angular to reference external imported values in templates
    
    also fix an import parsing bug getting the names and values backwards when using the `as` syntax
    
    * dev release
    
    * fix
    
    * fix string encoding for angular
    
    * safer encoding
    
    * release
    
    * fixes
    
    * release
    
    * lint
    
    * fixes
    
    * release
    
    * fix class strings
    
    * fix
    
    * release
    
    * fix for qwik to add preventdefault:event if seen directly in listener
    
    * snapshots

---
+++ packages/core/src/generators/qwik/add-prevent-default.ts
@@ -0,0 +1,25 @@
+import traverse from 'traverse';
+import { isMorphoNode } from '../../helpers/is-morpho-node';
+import { MorphoComponent } from '../../types/morpho-component';
+
+/**
+ * Find event handlers that explicitly call .preventDefault() and
+ * add preventdefault:event
+ * https://qwik.builder.io/tutorial/events/preventdefault
+ */
+export function addPreventDefault(json: MorphoComponent) {
+  traverse(json).forEach((node) => {
+    if (isMorphoNode(node)) {
+      if (node.bindings) {
+        for (const key of Object.keys(node.bindings)) {
+          if (key.startsWith('on')) {
+            if (node.bindings[key]?.code.includes('.preventDefault()')) {
+              const event = key.slice(2).toLowerCase();
+              node.properties['preventdefault:' + event] = '';
+            }
+          }
+        }
+      }
+    }
+  });
+}
