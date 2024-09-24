98b8c1ddee22d92831619bf8440069f57ddd2054
BuildWithMorpho <junaidgohan123@gmail.com>
2024-09-25 00:14:07 +0800

    feat: add support to specify targets or remove targets when running morpho build (#729)
    * feat(cli): add option --targets --targets-ignored to morpho build
    
    * docs(cli): add option targets doc snippet
    
    * docs(docs): add docs snippet of morpho-cli
    
    * feat: replace targetsIgnored with excludeTargets
    
    * docs(docs): improve docs of cli
    
    
    * docs(docs): improve docs of cli
    
    
    * chore: prettier

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -1,6 +1,22 @@
-import { GluegunCommand } from 'gluegun';
+import { GluegunCommand, GluegunParameters } from 'gluegun';
+import { MorphoConfig, Target } from '@builder.io/morpho';
 import { build } from '../build/build';
 import { getMorphoConfig } from '../helpers/get-morpho-config';
+
+const getTargets = (morphoConfig: MorphoConfig, cliOpts: GluegunParameters['options']) => {
+  const targetsFromCli: Target[] = (cliOpts.targets || '').split(',');
+  const excludeTargetsMap: Record<Target, true> = (cliOpts.excludeTargets || '')
+    .split(',')
+    .reduce((accu, t) => {
+      accu[t] = true;
+      return accu;
+    }, {});
+
+  const targets = Array.from(new Set([...morphoConfig.targets, ...targetsFromCli])).filter(
+    (t) => t && !excludeTargetsMap[t],
+  );
+  return targets;
+};
 
 const command: GluegunCommand = {
   name: 'build',
@@ -9,7 +25,12 @@ const command: GluegunCommand = {
     const { parameters } = toolbox;
     const opts = parameters.options;
     const configRelPath = opts.config ?? opts.c;
-    await build(getMorphoConfig(configRelPath));
+    const config = getMorphoConfig(configRelPath);
+    const targets = getTargets(config, opts);
+    await build({
+      ...config,
+      targets,
+    });
   },
 };
 
