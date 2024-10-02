da0eae4c8d81d2953e40f95fae73765da062114d
BuildWithMorpho <junaidgohan123@gmail.com>
2024-10-02 23:46:09 +0300

    morpho-811 added prefer-show-over-ternary-operator rule (#813)
    * morpho-811 added prefer-show-over-ternary-operator rule
    
    * morpho-811 improve checks
    
    * morpho-811 changed error msg
    
    * Update packages/eslint-plugin/package.json

---
+++ packages/eslint-plugin/src/rules/prefer-show-over-ternary-operator.ts
@@ -0,0 +1,51 @@
+import { types } from '@babel/core';
+import { Rule } from 'eslint';
+import isMorphoPath from '../helpers/isMorphoPath';
+
+// ------------------------------------------------------------------------------
+// Rule Definition
+// ------------------------------------------------------------------------------
+
+const rule: Rule.RuleModule = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: '<Show> is preferred over ternary expressions',
+      recommended: true,
+    },
+  },
+
+  create(context) {
+    // variables should be defined here
+    const filename = context.getFilename();
+
+    if (!isMorphoPath(filename)) return {};
+
+    // ----------------------------------------------------------------------
+    // Helpers
+    // ----------------------------------------------------------------------
+
+    // any helper functions should go here or else delete this section
+
+    // ----------------------------------------------------------------------
+    // Public
+    // ----------------------------------------------------------------------
+    //
+    const listener: Rule.RuleListener = {
+      ConditionalExpression(node) {
+        if (types.isJSXAttribute(node.parent.parent) && types.isJSXExpressionContainer(node.parent))
+          return;
+        if (types.isExpressionStatement(node.parent)) return;
+
+        context.report({
+          node,
+          message:
+            'Ternary expression support is minimal. Please use the Morpho `<Show>` component instead.',
+        });
+      },
+    };
+    return listener;
+  },
+};
+
+export default rule;
