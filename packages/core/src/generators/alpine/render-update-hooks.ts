ec5efdc89e80c35aeadf317b89db9c035123319c
BuildWithMorpho <junaidgohan123@gmail.com>
2024-10-06 12:14:52 -0500

    feat: Alpine.js generator (#846)
    * feat: Setup basic Alpine generator.
    
    * feat: Got `x-for`, `x-if`, and `x-data` working.
    
    * feat: Implemented `x-ref`.
    
    * fix: `x-bind` no longer uses the shorthand syntax when `useShorthandSyntax` is false.
    
    * fix: Handled `<Fragment/>` components.
    
    * feat: Can now adjust Alpine options in the fiddle.
    
    * refactor: Tidied up the code.
    
    * test: Added alpine test.
    
    * test: Added array to test all option combinations.
    
    * feat(cli): Added `alpine` as a compile target.
    
    * refactor: Removed homebrew `compose` function in favor of `lodash.flowRight`.
    
    * fix: Fixed spread bindings.
    
    * feat: Implemented `onMount` hook.
    
    * feat: `onUpdate` hook now works on the root element when `deps` is empty.
    
    * refactor: Moved `renderMountHook` to a separate file.
    
    * feat: Basic `$watch` hooks now get rendered.
    
    * fix: Fixed build error.
    
    * refactor(Fiddle): Moved the `Alpine.js` tab further down.
    
    * fix: Fixed issues with rendering `$watch` hooks.
    
    * fix(cli): build clean only targets directory files (#840)
    
    * fix(cli): build clean only targets directory
    
    * style: prettier code
    
    * feat: use options.getTargetPath
    
    * fix(qwik): remove generation of `mutable()` (#841)
    
    * fix(qwik): remove generation of `mutable()`
    
    * fixup! fix(qwik): remove generation of `mutable()`
    
    * fixup! fix(qwik): remove generation of `mutable()`
    
    * fix: camelcase attributes for react output (#844)
    
    * fix: camelcase attributes for react output
    
    * chore: remove console.log
    
    * chore: format code
    
    * chore: move unit test to shared tests
    
    * chore: update snapshots
    
    
    * publish (#849)
    
    * feat(angular): NgModules Generator (#845)
    
    
    * publish (#850)
    
    * remove templates (#854)
    
    * fix: Alpine components are now output with the `.ts` extension.
    
    * feat: Added initial `e2e` test for Alpine.js.
    
    * test: Updated snapshot.
    
    * fix: Fixed issue where alpine was outputing typescript to the html.
    
    * fix: Updated package name.
    
    * test: Added `alpine` to the playwright tests.
    
    * build(yarn): Updated lockfile to run immutably on linux.
    
    * style: Formatted code.
    
    * test: Updated snapshots.
    
    * test: Updated readme.
    
    * test: Updated alpine to acturally run shared tests.

---
+++ packages/core/src/generators/alpine/render-update-hooks.ts
@@ -0,0 +1,55 @@
+import { curry } from 'lodash';
+import { extendedHook, MorphoComponent } from '../../types/morpho-component';
+
+const extractCode = (hook: extendedHook) => hook.code;
+function renderRootUpdateHook(hooks: extendedHook[], output: string) {
+  if (hooks.length === 0) {
+    return output;
+  }
+  const str = `onUpdate() {
+        ${hooks.map(extractCode).join('\n')}
+    }`;
+
+  return output.replace(/,?(\s*})$/, `,\n${str}$1`);
+}
+
+function getRootUpdateHooks(json: MorphoComponent) {
+  return (json.hooks.onUpdate ?? []).filter((hook) => hook.deps == '');
+}
+
+export function hasRootUpdateHook(json: MorphoComponent): boolean {
+  return getRootUpdateHooks(json).length > 0;
+}
+
+export const renderUpdateHooks = curry((json: MorphoComponent, output: string) => {
+  return renderRootUpdateHook(getRootUpdateHooks(json), output);
+});
+
+function getWatchHooks(json: MorphoComponent) {
+  return (json.hooks.onUpdate ?? []).filter((hook) => hook.deps?.match(/state|this/));
+}
+
+export const hasWatchHooks = (json: MorphoComponent): boolean => {
+  return getWatchHooks(json).length > 0;
+};
+
+function renderWatchHook(hook: extendedHook): string {
+  const deps = (hook.deps ?? '')
+    ?.slice(1)
+    .slice(0, -1)
+    .split(', ')
+    .filter((dep) => dep.match(/state|this/));
+
+  return deps
+    .map(
+      (dep) =>
+        `this.$watch('${dep.replace(/(state|this)\./, '')}', (value, oldValue) => { ${
+          hook.code
+        } });`,
+    )
+    .join('\n');
+}
+
+export const renderWatchHooks = (json: MorphoComponent): string => {
+  return hasWatchHooks(json) ? getWatchHooks(json).map(renderWatchHook).join('\n') : '';
+};
