6e9043924cd73ac5e449cce3abb4e4fee35cb648
BuildWithMorpho <junaidgohan123@gmail.com>
2024-10-07 17:29:16 -0400

    Fix: imports (#907)
    * publish
    
    * remove readFile and transformImports from transpile
    
    * add type to example
    
    * fix import transform
    
    * fix react type generation
    
    * fix #900
    
    * update snapshots
    
    * update snapshots
    
    * fix tests

---
+++ packages/cli/src/build/helpers/options.ts
@@ -0,0 +1,14 @@
+import { MorphoConfig, Target } from '@builder.io/morpho';
+
+/**
+ * Output generated component file, before it is minified and transpiled into JS.
+ */
+export const checkShouldOutputTypeScript = ({
+  target,
+  options,
+}: {
+  target: Target;
+  options: MorphoConfig;
+}): boolean => {
+  return !!options.options[target]?.typescript;
+};
