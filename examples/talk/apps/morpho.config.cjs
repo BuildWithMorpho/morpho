1b5f0df69475f0472ac9a8dde657aede0c1c6517
BuildWithMorpho <junaidgohan123@gmail.com>
2024-10-12 05:24:34 +0100

    Feature/talk (#939)
    * setup Talk editor
    
    * more style tweaks
    
    * fix solid provider generation
    
    * cleanup qwik styles output
    
    * fix titles
    
    * cleanup generators code for simple cases
    
    * fix meta check
    
    * fix bug
    
    * more improvements
    
    * fix
    
    * add complete example
    
    * remove unused
    
    * extract generators from fiddle
    
    * fix
    
    * try to get ssr working in fiddle
    
    * fix ssr
    
    * add dark mode toggle
    
    * add dark mode toggle
    
    * talk examples
    
    * raise error for no config file
    
    * fix workspaces
    
    * yarn
    
    * fix prettier
    
    * fix styles
    
    * tweak examples
    
    * only clean unused files
    
    * add types to svelte
    
    * fix yarn
    
    * cleanup styles
    
    * remove logs
    
    * lock in ports
    
    * remove ts
    
    * update replaceIdentifiers to allow undefined `to`
    
    * update tests
    
    * pipe
    
    * remove unused file
    
    * wrap in try-catch
    
    * trim code
    
    * try to add stripIdentifiers for props
    
    * handle `to` fn param
    
    * extract fn
    
    * handle getters in babelTransformExpression
    
    * use babel for state replacer
    
    * document
    
    * try to stop trimming code. bad things happened
    
    * always show TS toggle
    
    * fix gitignore, add package.json
    
    * run snapupdate
    
    * add disableLinting prop
    
    * fix
    
    * fix .yarn gitignore
    
    * fix ci:build
    
    * undo CLI changes
    
    * fix types
    
    * rename
    
    * bring back some logging
    
    * cleanup
    
    * fix talk
    
    * fix talk template
    
    * try adding vertx

---
+++ examples/talk/apps/morpho.config.cjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import('@builder.io/morpho'.MorphoConfig)}
+ */
+module.exports = {
+  files: 'src/**',
+  targets: ['react', 'svelte'],
+  options: {
+    react: {
+      typescript: false,
+    },
+    svelte: {
+      typescript: true,
+    },
+  },
+};
