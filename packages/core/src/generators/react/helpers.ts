8176c50127f6ad0fa55364fb04a55b3e5c0d6b30
BuildWithMorpho <junaidgohan123@gmail.com>
2024-10-28 23:24:51 -0400

    Misc: refactor React code (#1028)
    * move code
    
    * fix

--- packages/core/src/generators/react/helpers.ts
+++ packages/core/src/generators/react/helpers.ts
@@ -1,3 +1,5 @@
+import { MorphoComponent } from '../../types/morpho-component';
+import { MorphoNode } from '../../types/morpho-node';
 import { stripStateAndPropsRefs } from '../../helpers/strip-state-and-props-refs';
 
 import { ToReactOptions } from './types';
@@ -12,3 +14,11 @@ export const processBinding = (str: string, options: ToReactOptions) => {
     includeProps: false,
   });
 };
+
+export const openFrag = (options: ToReactOptions) => getFragment('open', options);
+export const closeFrag = (options: ToReactOptions) => getFragment('close', options);
+export function getFragment(type: 'open' | 'close', options: ToReactOptions) {
+  const tagName = options.preact ? 'Fragment' : '';
+  return type === 'open' ? `<${tagName}>` : `</${tagName}>`;
+}
+export const wrapInFragment = (json: MorphoComponent | MorphoNode) => json.children.length !== 1;
