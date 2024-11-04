46c2cf00f6e8af190bb6a91146715dbe5467b525
BuildWithMorpho <junaidgohan123@gmail.com>
2024-11-04 16:28:45 -0500

    Fix e2e tests (#1054)
    * fix qwik $
    
    * tidy
    
    * remove
    
    * remove
    
    * init new workflow
    
    * add react
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * add
    
    * fix
    
    * add
    
    * fdsa
    
    * fjdskal
    
    * fix
    
    * update
    
    * fdsa
    
    * fix
    
    * fdsa
    
    * fdas
    
    * fdsa
    
    * clean
    
    * setup vitest in cli
    
    * fix
    
    * fix
    
    * vitest
    
    * setup vitest
    
    * lean
    
    * clean
    
    * yaya
    
    * ok
    
    * revert
    
    * log
    
    * test
    
    * Revert "test"
    
    This reverts commit 273ec7e57a3973c18b93402a41c847a3a2c7fb7c.
    
    * fix
    
    * fda
    
    * fix vue e2e
    
    * fix
    
    * enable
    
    * fdsa
    
    * fix
    
    * fda
    
    * clean
    
    * fix
    
    * f
    
    * f
    
    * fdsa
    
    * fdsa

--- packages/cli/src/helpers/get-morpho-config.ts
+++ packages/cli/src/helpers/get-morpho-config.ts
@@ -6,9 +6,12 @@ import { MorphoConfig } from '@builder.io/morpho';
  * @param relPath { string } the relative path from pwd to config-file
  */
 export function getMorphoConfig(relPath?: string): MorphoConfig | null {
-  const path = resolve(process.cwd(), relPath || 'morpho.config.js');
-  if (fs.existsSync(path)) {
-    const module = require(path);
+  const path = resolve(process.cwd(), relPath || 'morpho.config');
+  if (fs.existsSync(path + '.js')) {
+    const module = require(path + '.js');
+    return module?.default || module || null;
+  } else if (fs.existsSync(path + '.cjs')) {
+    const module = require(path + '.cjs');
     return module?.default || module || null;
   }
 
