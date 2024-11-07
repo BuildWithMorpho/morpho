199c9517b4a68c8f0ec6b5ad1ab933c5ce571742
BuildWithMorpho <junaidgohan123@gmail.com>
2024-11-07 15:46:22 -0700

    Fix: if root is a text node it needs to be wrapped in a fragment for JSX outputs (#1073)
    * Fix: if root is a text node it needs to be wrapped in a fragment for JSX outputs
    
    * Fixes
    
    * Fix import paths

---
+++ packages/core/src/helpers/is-root-text-node.ts
@@ -0,0 +1,11 @@
+import { MorphoNode } from '../types/morpho-node';
+import { MorphoComponent } from '../types/morpho-component';
+
+export function isRootTextNode(json: MorphoComponent) {
+  const firstChild = json.children[0];
+  return Boolean(firstChild && isTextNode(firstChild));
+}
+
+export function isTextNode(node: MorphoNode) {
+  return Boolean(node.properties._text || node.bindings._text);
+}
