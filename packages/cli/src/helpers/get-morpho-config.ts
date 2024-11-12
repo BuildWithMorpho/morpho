8d70dc613a5123d3ee6f9a8903468817b249c99a
BuildWithMorpho <junaidgohan123@gmail.com>
2024-11-12 22:21:50 +0300

    fix: no config file found for Morpho (#1115)

--- packages/cli/src/helpers/get-morpho-config.ts
+++ packages/cli/src/helpers/get-morpho-config.ts
@@ -1,17 +1,31 @@
+import { MorphoConfig } from '@builder.io/morpho';
 import fs from 'fs';
 import { resolve } from 'path';
-import { MorphoConfig } from '@builder.io/morpho';
 
 /**
  * @param relPath { string } the relative path from pwd to config-file
  */
 export function getMorphoConfig(relPath?: string): MorphoConfig | null {
-  const path = resolve(process.cwd(), relPath || 'morpho.config');
-  if (fs.existsSync(path + '.js')) {
-    const module = require(path + '.js');
-    return module?.default || module || null;
-  } else if (fs.existsSync(path + '.cjs')) {
-    const module = require(path + '.cjs');
+  if (!relPath) {
+    const path = resolve(process.cwd(), 'morpho.config');
+
+    if (fs.existsSync(path + '.js')) {
+      const module = require(path + '.js');
+      return module?.default || module || null;
+    }
+
+    if (fs.existsSync(path + '.cjs')) {
+      const module = require(path + '.cjs');
+      return module?.default || module || null;
+    }
+
+    return null;
+  }
+
+  const path = resolve(process.cwd(), relPath);
+
+  if (fs.existsSync(path)) {
+    const module = require(path);
     return module?.default || module || null;
   }
 
