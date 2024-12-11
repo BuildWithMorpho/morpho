0de55aad68aa10a0030b0ca53e8277d7b96499c7
BuildWithMorpho <junaidgohan123@gmail.com>
2024-12-11 12:49:11 -0300

    Fix/imports (#1254)
    * fix
    
    * add js extension to context imports
    
    * move more to core
    
    * f
    
    * k
    
    * release
    
    * f
    
    * f
    
    * update snapshots
    
    * f

---
+++ packages/core/src/helpers/output.ts
@@ -0,0 +1,15 @@
+import { MorphoConfig, Target } from '@builder.io/morpho';
+import { checkIsDefined } from './nullable';
+
+export const checkShouldOutputTypeScript = ({
+  target,
+  options,
+}: {
+  target: Target;
+  options: MorphoConfig;
+}): boolean => {
+  const targetTsConfig = options.options[target]?.typescript;
+  return checkIsDefined(targetTsConfig)
+    ? targetTsConfig
+    : options.commonOptions?.typescript || false;
+};
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,7 +2,6 @@ import { JSX } from '@builder.io/morpho/jsx-runtime';
 import { Dictionary } from './helpers/typescript';
 import { ComponentMetadata } from './types/metadata';
 import { ReactivityType, TargetBlock } from './types/morpho-component';
-
 export * from './flow';
 export * from './generators/alpine';
 export * from './generators/angular';
@@ -29,7 +28,16 @@ export * from './generators/swift-ui';
 export * from './generators/taro';
 export * from './generators/template';
 export * from './generators/vue';
+export {
+  checkIsLiteComponentFilePath,
+  checkIsMorphoComponentFilePath,
+  checkIsSvelteComponentFilePath,
+  getComponentFileExtensionForTarget,
+  renameComponentFile,
+  renameImport,
+} from './helpers/component-file-extensions';
 export * from './helpers/is-morpho-node';
+export { checkShouldOutputTypeScript } from './helpers/output';
 export * from './helpers/signals';
 export * from './helpers/typescript-project';
 export * from './parsers/angular';
