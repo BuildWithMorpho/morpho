9f689035ea9719c43e1a5d8b9363d77759775be6
BuildWithMorpho <junaidgohan123@gmail.com>
2024-12-31 07:03:07 -0300

    Fix: RSC check (#1287)
    * fix commonOptions.ts config
    
    * f
    
    * f
    
    * on-event-arg
    
    * f
    
    * fix
    
    * f
    
    * f
    
    * f
    
    * improve sfds
    
    * rl
    
    * rlrl
    
    * fix and move rsc logic
    
    * f
    
    * f
    
    * fix

---
+++ packages/core/src/generators/helpers/rsc.ts
@@ -0,0 +1,29 @@
+import traverse from 'traverse';
+import { isMorphoNode } from '../../helpers/is-morpho-node';
+import type { MorphoComponent } from '../../types/morpho-component';
+import type { MorphoNode } from '../../types/morpho-node';
+
+const checkIsNodeAMorphoComponent = (node: MorphoNode) =>
+  node.name[0] === node.name[0].toUpperCase();
+
+export const checkIfIsClientComponent = (json: MorphoComponent) => {
+  if (json.hooks.onMount.length) return true;
+  if (json.hooks.onUnMount?.code) return true;
+  if (json.hooks.onUpdate?.length) return true;
+  if (Object.keys(json.refs).length) return true;
+  if (Object.keys(json.context.set).length) return true;
+  if (Object.keys(json.context.get).length) return true;
+  if (Object.values(json.state).filter((s) => s?.type === 'property').length) return true;
+
+  let foundEventListener = false;
+  traverse(json).forEach(function (node) {
+    if (isMorphoNode(node) && !checkIsNodeAMorphoComponent(node)) {
+      if (Object.keys(node.bindings).filter((item) => item.startsWith('on')).length) {
+        foundEventListener = true;
+        this.stop();
+      }
+    }
+  });
+
+  return foundEventListener;
+};
