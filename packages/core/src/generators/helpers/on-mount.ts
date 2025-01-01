7876f140fbe6458c9e997a6388f7b2bb6f2f9454
BuildWithMorpho <junaidgohan123@gmail.com>
2025-01-01 17:18:21 -0400

    Fix: qwik events $ (#1296)
    * f
    
    * f
    
    * l
    
    * f
    
    * rl
    
    * fix react
    
    * rl
    
    * fix vue bindings
    
    * snp
    
    * rl

--- packages/core/src/generators/helpers/on-mount.ts
+++ packages/core/src/generators/helpers/on-mount.ts
@@ -16,7 +16,7 @@ export const stringifySingleScopeOnMount = (json: MorphoComponent) => {
     .map((hook, i) => {
       const hookFnName = `onMountHook_${i}`;
       return `
-    function ${hookFnName}() {
+    const ${hookFnName} = () => {
       ${hook.code}
     }
     ${hookFnName}();`;
