616dd4be4294b27bcc4fe5d8a67bb65099ebeb1c
BuildWithMorpho <junaidgohan123@gmail.com>
2025-03-28 21:32:05 -0300

    Builder: Fix index rename in For  (#1617)
    * 🪩
    
    * 👹
    
    * 🪩
    
    * 🏃
    
    * tests
    
    * 🪩
    
    * 🏃

---
+++ .changeset/dry-dodos-sin.md
@@ -0,0 +1,5 @@
+---
+'@builder.io/morpho': patch
+---
+
+[Builder] Fix: do not rename vars within For that are shadowing the loop's index
--- packages/core/src/helpers/bindings.ts
+++ packages/core/src/helpers/bindings.ts
@@ -1,6 +1,11 @@
 import { Binding } from '../types/morpho-node';
 
-export const createSingleBinding = (args: Omit<Binding, 'type'>): Binding => ({
+type SingleBinding = Omit<Binding & { type: 'single' }, 'type'>;
+
+export const createSingleBinding = (
+  args: Omit<SingleBinding, 'bindingType'> & Partial<Pick<SingleBinding, 'bindingType'>>,
+): Binding => ({
   ...args,
+  bindingType: args.bindingType || 'expression',
   type: 'single',
 });
