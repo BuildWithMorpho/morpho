28ef63cfde75e8e6c7165a64e1a050ffe9e38c30
BuildWithMorpho <junaidgohan123@gmail.com>
2025-04-30 04:55:12 -0700

    properly encode ">", "<", and other necessary chars in morpho JSX (#1718)
    * properly encode ">" and "<" in morpho JSX
    
    * changeset
    
    * more
    
    * fix snap
    
    * fixed

--- packages/core/src/__tests__/morpho.test.ts
+++ packages/core/src/__tests__/morpho.test.ts
@@ -1,4 +1,6 @@
 import { componentToMorpho } from '@/generators/morpho';
+import { createMorphoComponent } from '@/helpers/create-morpho-component';
+import { createMorphoNode } from '@/helpers/create-morpho-node';
 import { runTestsForTarget } from './test-generator';
 
 describe('Morpho, format: legacy', () => {
@@ -29,5 +31,36 @@ describe('Morpho, format: react', () => {
     },
     target: 'morpho',
     generator: componentToMorpho,
+  });
+});
+
+describe('Can encode <> in text', () => {
+  it('should encode <> in text', () => {
+    const result = componentToMorpho()({
+      component: createMorphoComponent({
+        children: [
+          createMorphoNode({
+            properties: { _text: '<>{}' },
+          }),
+        ],
+        hooks: {},
+      }),
+    });
+
+    expect(result).toMatchSnapshot();
+  });
+  it('should not encode valid jsx', () => {
+    const result = componentToMorpho()({
+      component: createMorphoComponent({
+        children: [
+          createMorphoNode({
+            properties: { _text: 'hello <b>world</b>' },
+          }),
+        ],
+        hooks: {},
+      }),
+    });
+
+    expect(result).toMatchSnapshot();
   });
 });
