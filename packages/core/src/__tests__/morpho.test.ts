d0b24d116123792162f3afe1e07355f53e67440a
BuildWithMorpho <junaidgohan123@gmail.com>
2025-05-05 08:59:55 -0400

    fix: < and > are properly escaped when generating JSX (#1742)

--- packages/core/src/__tests__/morpho.test.ts
+++ packages/core/src/__tests__/morpho.test.ts
@@ -65,6 +65,26 @@ describe('Can encode <> in text', () => {
     expect(result).toMatchSnapshot();
   });
 
+  it('encode single > character', () => {
+    const result = componentToMorpho()({
+      component: createMorphoComponent({
+        children: [
+          createMorphoNode({
+            properties: { _text: '>' },
+          }),
+        ],
+        hooks: {},
+      }),
+    });
+
+    expect(result).toMatchInlineSnapshot(`
+      "export default function MyComponent(props) {
+        return <>&amp;gt;</>;
+      }
+      "
+    `);
+  });
+
   it('should not output invalid jsx attributes', () => {
     const result = componentToMorpho()({
       component: createMorphoComponent({
